0d6ab717794a756405f3bbce29dce85c458a23bf
Zaur <z.tsoroev@original-group.ru>
2023-10-08 16:18:07 +0300

    added modal, portal

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useCallback, useEffect } from 'react';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import { Portal } from 'shared/ui/Portal/Portal';
+import { useTheme } from 'app/providers/ThemeProvider';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+  className?: string;
+  children?: React.ReactNode;
+  isOpen?: boolean;
+  onClose?: () => void;
+}
+
+export const Modal:FC<ModalProps> = (props) => {
+  const {
+    className, children, isOpen, onClose,
+  } = props;
+
+  const { theme } = useTheme();
+
+  const closeHandler = useCallback(() => {
+    if (onClose) onClose();
+  }, [onClose]);
+
+  const onKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.key === 'Escape') closeHandler();
+  }, [closeHandler]);
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', onKeyDown);
+    }
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [isOpen, onKeyDown]);
+
+  const onContentClick = (e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation();
+
+  const mods: Record<string, boolean> = {
+    [cls.opened]: isOpen,
+    [cls[theme]]: true,
+  };
+
+  return (
+    <Portal>
+      <div className={classNames(cls.modal, mods, [className])}>
+        <div className={cls.overlay} onClick={closeHandler}>
+          <div className={cls.content} onClick={onContentClick}>
+            {children}
+          </div>
+        </div>
+      </div>
+    </Portal>
+
+  );
+};
